74b2dbf2fe8bc6f2d4f7145322b68bb0f882c812
Saqib Usman <saqib.usman@audibene.de>
2022-11-30 20:25:15 +0100

    Jetpack compose tutorial

---
+++ app/src/main/java/com/playground/compose/MainActivity.kt
@@ -0,0 +1,223 @@
+package com.playground.compose
+
+import android.content.res.Configuration
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.animation.animateColorAsState
+import androidx.compose.animation.animateContentSize
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.*
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.playground.compose.ui.theme.ComposeTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            ComposeTheme {
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colors.background
+                ) {
+//                    MessageCard(message = ChatMessage("asd", "ASd"))
+                    Conversion(SampleData.conversationSample)
+                }
+
+            }
+
+            //MessageCard(name = "Android")
+//            ComposeTheme {
+//                // A surface container using the 'background' color from the theme
+//                Surface(
+//                    modifier = Modifier.fillMaxSize(),
+//                    color = MaterialTheme.colors.background
+//                ) {
+//                    Greeting("Android")
+//                }
+//            }
+        }
+    }
+}
+
+data class ChatMessage(val author: String, val message: String) 
+
+@Composable
+fun MessageCard(message: ChatMessage) {
+    Row(modifier = Modifier.padding(8.dp)) {
+        Image(
+            painter = painterResource(id = R.drawable.profile_picture),
+            contentDescription = "author picture",
+            modifier = Modifier
+                .size(40.dp)
+                .border(1.5.dp, MaterialTheme.colors.secondary, CircleShape)
+                .clip(CircleShape)
+        )
+        Spacer(modifier = Modifier.width(8.dp))
+
+        var isExpanded by remember { mutableStateOf(false) }
+        val surfaceColor by animateColorAsState(if (isExpanded) MaterialTheme.colors.primary else MaterialTheme.colors.surface)
+
+        Column(modifier = Modifier.clickable { isExpanded = !isExpanded }) {
+            Text(
+                text = message.author,
+                color = MaterialTheme.colors.secondaryVariant,
+                style = MaterialTheme.typography.subtitle2
+            )
+            Spacer(modifier = Modifier.height(4.dp))
+            Surface(
+                shape = MaterialTheme.shapes.medium,
+                elevation = 1.dp,
+                color = surfaceColor,
+                modifier = Modifier.animateContentSize().padding(1.dp)
+            ) {
+                Text(
+                    text = message.message,
+                    maxLines = if (isExpanded) Int.MAX_VALUE else 1,
+                    style = MaterialTheme.typography.body2,
+                    modifier = Modifier.padding(all = 4.dp)
+                )
+            }
+
+        }
+    }
+}
+
+@Composable
+fun Conversion(messages: List<ChatMessage>) {
+    LazyColumn {
+        messages.map { item { MessageCard(it) } }
+    }
+}
+
+
+@Preview
+@Composable
+fun PreviewConversion() {
+    Conversion(SampleData.conversationSample)
+
+}
+
+object SampleData {
+    // Sample conversation data
+    val conversationSample = listOf(
+        ChatMessage(
+            "Colleague",
+            "Test...Test...Test..."
+        ),
+        ChatMessage(
+            "Colleague",
+            "List of Android versions:\n" +
+                    "Android KitKat (API 19)\n" +
+                    "Android Lollipop (API 21)\n" +
+                    "Android Marshmallow (API 23)\n" +
+                    "Android Nougat (API 24)\n" +
+                    "Android Oreo (API 26)\n" +
+                    "Android Pie (API 28)\n" +
+                    "Android 10 (API 29)\n" +
+                    "Android 11 (API 30)\n" +
+                    "Android 12 (API 31)\n"
+        ),
+        ChatMessage(
+            "Colleague",
+            "I think Kotlin is my favorite programming language.\n" +
+                    "It's so much fun!"
+        ),
+        ChatMessage(
+            "Colleague",
+            "Searching for alternatives to XML layouts..."
+        ),
+        ChatMessage(
+            "Colleague",
+            "Hey, take a look at Jetpack Compose, it's great!\n" +
+                    "It's the Android's modern toolkit for building native UI." +
+                    "It simplifies and accelerates UI development on Android." +
+                    "Less code, powerful tools, and intuitive Kotlin APIs :)"
+        ),
+        ChatMessage(
+            "Colleague",
+            "It's available from API 21+ :)"
+        ),
+        ChatMessage(
+            "Colleague",
+            "Writing Kotlin for UI seems so natural, Compose where have you been all my life?"
+        ),
+        ChatMessage(
+            "Colleague",
+            "Android Studio next version's name is Arctic Fox"
+        ),
+        ChatMessage(
+            "Colleague",
+            "Android Studio Arctic Fox tooling for Compose is top notch ^_^"
+        ),
+        ChatMessage(
+            "Colleague",
+            "I didn't know you can now run the emulator directly from Android Studio"
+        ),
+        ChatMessage(
+            "Colleague",
+            "Compose Previews are great to check quickly how a composable layout looks like"
+        ),
+        ChatMessage(
+            "Colleague",
+            "Previews are also interactive after enabling the experimental setting"
+        ),
+        ChatMessage(
+            "Colleague",
+            "Have you tried writing build.gradle with KTS?"
+        ),
+    )
+}
+
+
+@Preview(name = "Light Mode")
+@Preview(
+    uiMode = Configuration.UI_MODE_NIGHT_YES,
+    showBackground = true,
+    name = "Dark Mode"
+)
+@Composable
+fun PreviewMessageCard() {
+    ComposeTheme {
+        Surface {
+            MessageCard(ChatMessage("authoer1", "message1"))
+        }
+    }
+
+}
+
+//@Preview
+//@Composable
+//fun PreviewMessageCard() {
+//    MessageCard(name = "Saqib Usman2")
+//}
+//
+//@Composable
+//fun MessageCard(name: String) {
+//    Text(text = "Hello $name")
+//}
+//
+//@Composable
+//fun Greeting(name: String) {
+//    Text(text = "Hello $name!")
+//}
+//
+//@Preview(showBackground = true)
+//@Composable
+//fun DefaultPreview() {
+//    ComposeTheme {
+//        Greeting("Android")
+//    }
+//}
